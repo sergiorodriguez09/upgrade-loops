dfde00e54f649b2bcc4da3b46f639775541884df
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-10 18:47:52 +0200

    Iteracion 2

--- Iteration2.js
+++ Iteration2.js
@@ -1,4 +1,4 @@
-//! Iteración #2: Condicionales avanzados
+//!  Iteración #2: Condicionales avanzados
 
 //Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log//
 
